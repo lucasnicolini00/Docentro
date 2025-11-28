bf98251ff41f3ea6fc49aee44b4e53b950e37321
Lucas Nicolini <lucasnicolini00@gmail.com>
2025-11-28 15:14:01 +0000

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
-
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -21,6 +21,7 @@ export default function RootLayout({
       >
         {children}
         <Toaster position="bottom-right" />
+        <Analytics />
       </body>
     </html>
   );
